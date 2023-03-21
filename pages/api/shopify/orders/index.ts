ae65d4536f32352f3f3be7a709e32daa04d2c3c4
Dylan <dylan.p@tiflabs.in>
2023-03-21 19:46:38 +0530

    >Orders api to add to db new orders

--- pages/api/shopify/orders/index.ts
+++ pages/api/shopify/orders/index.ts
@@ -89,144 +89,6 @@ const getAllOrders = async (orders = [], after = null, timeout = 100) => {
   try {
     await sleep(timeout)
     const data = await graphQLClient.request(ordersQuery, after ? { after } : {})
-    const { customer, shippingAddress, billingAddress, lineItems } = data.orders.nodes[0]
-
-    // console.log("data456: ", JSON.stringify(data,null,2) )
-    // return
-    const newOrderObject = {
-      customer: {
-        firstName: customer.firstName,
-        lastName: customer.lastName,
-        shopifyId: customer.id,
-        addresses: {
-          create: {
-            areaStreet: customer.defaultAddress.address1,
-            landmarkName: customer.defaultAddress.address2,
-            cityCountryProvince: customer.defaultAddress.city,
-            state: customer.defaultAddress.province,
-            pincode: customer.defaultAddress.zip,
-            country: 1,
-            contact_number: {
-              create: [
-                {
-                  type: "mobile",
-                  number: customer.defaultAddress.phone,
-                },
-              ],
-            },
-          },
-        },
-      },
-      order: {
-        shopifyId: data.orders.nodes[0].id,
-        orderStatus: 4,
-        isShippingIsBilling: false,
-        shippingAddress: {
-          areaStreet: shippingAddress.address1,
-          landmarkName: shippingAddress.address2,
-          cityCountryProvince: shippingAddress.city,
-          state: shippingAddress.province,
-          pincode: shippingAddress.zip,
-          country: 1,
-          contact_number: {
-            create: [
-              {
-                type: "mobile",
-                number: shippingAddress.phone,
-              },
-            ],
-          },
-        },
-        billingAddress: {
-          areaStreet: billingAddress.address1,
-          landmarkName: billingAddress.address2,
-          cityCountryProvince: billingAddress.city,
-          state: billingAddress.province,
-          pincode: billingAddress.zip,
-          country: 1,
-          contact_number: {
-            create: [
-              {
-                type: "mobile",
-                number: billingAddress.phone,
-              },
-            ],
-          },
-        },
-        paymentStatus: data.orders.nodes[0].displayFinancialStatus,
-        totalPrice: parseFloat(data.orders.nodes[0].totalPrice),
-        gateway: data.orders.nodes[0].paymentGatewayNames?.join(","),
-        order_items: {
-          create: lineItems.nodes.map((lineItem) => ({
-            product: 11,
-            quantity: lineItem.quantity,
-            price: parseFloat(lineItem.discountedTotalSet.shopMoney.amount),
-          })),
-        },
-      },
-    }
-
-    await createOrderFunction({
-      ...newOrderObject,
-      order: {
-        ...newOrderObject.order,
-        order_items: {
-          create: lineItems.nodes.map((lineItem, index) => ({
-            product: 11 + index,
-            quantity: lineItem.quantity,
-            price: 123,
-          })),
-        },
-      },
-    })
-
-    return
-
-    const createShopify = await db.shopify.create({
-      data: {
-        orderId: newOrderObject.shopifyId,
-        orderNumber: newOrderObject.shopifyId,
-        orderStatusUrl: newOrderObject.shopifyId,
-      },
-    })
-
-    const createAddress = await db.addresses.create({
-      data: newOrderObject.customer.addresses,
-    })
-
-    const Customer = await db.customers.create({
-      data: {
-        firstName: newOrderObject.customer.firstName,
-        lastName: newOrderObject.customer.lastName,
-        shopifyId: newOrderObject.customer.id,
-        addressesId: createAddress.id,
-      },
-    })
-
-    const ShippingAddress = await db.addresses.create({
-      data: newOrderObject.order.shippingAddress,
-    })
-
-    const BillingAddress = await db.addresses.create({
-      data: newOrderObject.order.billingAddress,
-    })
-
-    const order = await db.orders.create({
-      data: {
-        shopifyId: createShopify.id,
-        orderStatus: newOrderObject.order.orderStatus,
-        paymentStatus: newOrderObject.order.paymentStatus,
-        totalPrice: newOrderObject.order.totalPrice,
-        gateway: newOrderObject.order.gateway,
-        customerId: Customer.id,
-        shippingAddressId: ShippingAddress.id,
-        billingAddressId: BillingAddress.id,
-        order_items: newOrderObject.order.order_items,
-      },
-    })
-    console.log("order: ", order)
-
-    return
     const foundIndex = data.orders.nodes.findIndex((data) => data.id === latestOrder.id)
     if (!data?.orders?.nodes?.length) {
       console.log("No more from shopify")
@@ -254,19 +116,88 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.time()
 
   const orders = await getAllOrders()
+  await Promise.all(
+    orders.map(async (order) => {
+      const { customer, shippingAddress, billingAddress, lineItems } = order
+      const newOrderObject = {
+        customer: {
+          firstName: customer.firstName,
+          lastName: customer.lastName,
+          shopifyId: customer.id,
+          addresses: {
+            create: {
+              areaStreet: customer.defaultAddress.address1,
+              landmarkName: customer.defaultAddress.address2,
+              cityCountryProvince: customer.defaultAddress.city,
+              state: customer.defaultAddress.province,
+              pincode: customer.defaultAddress.zip,
+              country: 1,
+              contact_number: {
+                create: [
+                  {
+                    type: "mobile",
+                    number: customer.defaultAddress.phone,
+                  },
+                ],
+              },
+            },
+          },
+        },
+        order: {
+          shopifyId: order.id,
+          orderStatus: 4,
+          isShippingIsBilling: false,
+          shippingAddress: {
+            areaStreet: shippingAddress.address1,
+            landmarkName: shippingAddress.address2,
+            cityCountryProvince: shippingAddress.city,
+            state: shippingAddress.province,
+            pincode: shippingAddress.zip,
+            country: 1,
+            contact_number: {
+              create: [
+                {
+                  type: "mobile",
+                  number: shippingAddress.phone,
+                },
+              ],
+            },
+          },
+          billingAddress: {
+            areaStreet: billingAddress.address1,
+            landmarkName: billingAddress.address2,
+            cityCountryProvince: billingAddress.city,
+            state: billingAddress.province,
+            pincode: billingAddress.zip,
+            country: 1,
+            contact_number: {
+              create: [
+                {
+                  type: "mobile",
+                  number: billingAddress.phone,
+                },
+              ],
+            },
+          },
+          paymentStatus: order.displayFinancialStatus,
+          totalPrice: parseFloat(order.totalPrice),
+          gateway: order.paymentGatewayNames?.join(","),
+          order_items: {
+            create: lineItems.nodes.map((lineItem) => ({
+              product: 11,
+              quantity: lineItem.quantity,
+              price: parseFloat(lineItem.discountedTotalSet.shopMoney.amount),
+            })),
+          },
+        },
+      }
+
+      await createOrderFunction(newOrderObject)
+    })
+  )
+
   console.timeEnd()
 
   console.log("orders: ", orders)
-  // const session = await getSession(req, res)
-  // if (req.method === "GET") {
-  //   if (session.$isAuthorized()) {
-  //     console.log("here")
-  //   } else {
-  //     res.status(404)
-  //   }
-  // } else {
-  //   // Handle any other HTTP method
-  //   res.status(404)
-  // }
 }
 export default handler
